c3a9ea4d0b24e447e5caefd7f8361c7cbf565eae
Nyasha <nynyashadzashe@gmail.com>
2023-05-04 13:28:23 +0200

    update

--- Projects/Nyasha Image Extracter V2/main.py
+++ Projects/Nyasha Image Extracter V2/main.py
@@ -20,4 +20,23 @@ for page_num in range(page_nums):
     page_content = pdf_file[page_num]
     images_list.extend(page_content.get_images())
 
-print(images_list)
+#Raise error if PDF has no images
+if len(images_list)==0:
+    raise ValueError(f'No images found in {file_path}')
+
+#Save all the extracted images
+for i, image in enumerate(images_list, start=1):
+    #Extract the image object number
+    xref = image[0]
+    #Extract image
+    base_image = pdf_file.extract_image(xref)
+    #Store image bytes
+    image_bytes = base_image['image']
+    #Store image extension
+    image_ext = base_image['ext']
+    #Generate image file name
+    image_name = str(i) + '.' + image_ext
+    #Save image
+    with open(os.path.join('images/', image_name) , 'wb') as image_file:
+        image_file.write(image_bytes)
+        image_file.close()
